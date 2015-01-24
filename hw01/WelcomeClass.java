54fdb59d594e235b0b5a20ba100576b849c233cb
kah217 <kah217@lehigh.edu>
2015-01-24 00:26:07 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Karen Huang
+//
+//Welcome Class Java Program
+//
+//      first compile the program
+//          javac WelcomeClass.java
+//      run the program
+//          java WelcomeClass//
+        
+
+//define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//print the statement with box around it
+        System.out.println("  -----------");
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-K--A--H--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This winter break, I went to Colombia. The food was delicious.");      
+        
+        //test/testtest
+    }
+}
